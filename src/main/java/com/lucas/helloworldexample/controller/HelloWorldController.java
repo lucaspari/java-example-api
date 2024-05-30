e6ead31097fd374551175e88d28ece23e8a84824
lucaspari <lucas.pari@icloud.com>
2024-05-30 13:42:10 -0300

    feat: added swagger

--- src/main/java/com/lucas/helloworldexample/controller/HelloWorldController.java
+++ src/main/java/com/lucas/helloworldexample/controller/HelloWorldController.java
@@ -1,18 +1,17 @@
 package com.lucas.helloworldexample.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Hello World", description = "Hello World Controller")
 public class HelloWorldController {
 
     @GetMapping("/hello")
+    @Operation(summary = "Hello World", description = "Returns a simple Hello World message")
     public String hello() {
         return "Hello World";
-    }
-
-    @GetMapping("/hello2")
-    public String hello2() {
-        return "Hello World 2";
     }
 }
